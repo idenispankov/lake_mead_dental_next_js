9b249f927bdfb3a05f7205acc0956d348899421b
Denis Pankov <idenispankov@gmail.com>
2021-07-20 13:13:37 -0700

    fixed hamburger menu position onClick

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import navbarStyles from '../styles/Navbar.module.css';
 import phoneIcon from '../public/images/phone_icon.svg';
 import mailIcon from '../public/images/mail_icon.svg';
 import Image from 'next/image';
 import Link from 'next/link';
+import HamburgerMenu from 'react-hamburger-menu';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className={navbarStyles.header}>
       <div className={navbarStyles.header__contact}>
@@ -100,6 +108,66 @@ export default function Navbar() {
               </Link>
             </li>
           </ul>
+          <HamburgerMenu
+            className={navbarStyles.hamburger}
+            isOpen={isOpen}
+            menuClicked={handleClick}
+            color='#3393f0'
+            width={40}
+            height={30}
+            strokeWidth={4}
+            rotate={0}
+            borderRadius={10}
+            animationDuration={0.5}
+          />
+
+          {isOpen && (
+            <div className={navbarStyles.hamburger__container}>
+              <ul className={navbarStyles.hamburger__list}>
+                <li>
+                  <Link href='/'>
+                    <a className={navbarStyles.hamburger__list_item}>Home</a>
+                  </Link>
+                  <Link href='/new-patients'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      New Patients
+                    </a>
+                  </Link>
+                  <Link href='/services'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      Services
+                    </a>
+                  </Link>
+                  <Link href='/meet-the-doctors'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      Meet The Doctors
+                    </a>
+                  </Link>
+                  <Link href='/insurance'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      Insurance
+                    </a>
+                  </Link>
+                  <Link href='/gallery'>
+                    <a className={navbarStyles.hamburger__list_item}>Gallery</a>
+                  </Link>
+                  <Link href='/covid-19'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      Covid-19
+                    </a>
+                  </Link>
+                  <Link href='/faq'>
+                    <a className={navbarStyles.hamburger__list_item}>FAQ</a>
+                  </Link>
+                  <Link href='/contact-us'>
+                    <a className={navbarStyles.hamburger__list_item}>
+                      Contact us
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </header>
