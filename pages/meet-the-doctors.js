3c5ac60e2571ecb8df9182c5694d5d1ccf0bbb85
Denis Pankov <idenispankov@gmail.com>
2021-07-23 14:01:04 -0700

    heading for meet the doctor section

--- pages/meet-the-doctors.js
+++ pages/meet-the-doctors.js
@@ -1,5 +1,19 @@
-import meetTheDoctorsStyles from '../styles/MeetTheDoctors.module.css';
+import styles from '../styles/MeetTheDoctor.module.css';
+import Section from '../components/Section';
+import FlexContainer from '../components/FlexContainer';
 
 export default function MeetTheDoctors() {
-  return <div>Meet The Doctors</div>;
+  return (
+    <Section>
+      <div className={styles.heading__container}>
+        <h2 className={styles.heading}>Meet The Doctors</h2>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+          dicta molestiae repudiandae nisi nesciunt cupiditate?
+        </p>
+      </div>
+      <FlexContainer optionalClass={styles.flex__container}></FlexContainer>
+      <FlexContainer optionalClass={styles.flex__container}></FlexContainer>
+    </Section>
+  );
 }
