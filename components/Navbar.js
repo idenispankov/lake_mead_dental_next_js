691a195de327feec08ca252b4b31a9a4ecf70d57
Denis Pankov <idenispankov@gmail.com>
2021-07-27 10:48:39 -0700

    navbar, header are ready - responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -44,102 +44,104 @@ export default function Navbar() {
       </div>
 
       <nav className={styles.navbar}>
-        <Link href='/'>
-          <a className={styles.logo}>
-            <div className={styles.logo__image}>
-              <Image src={logo} alt='logo' />
-            </div>
-            <p className={styles.logo__text}>
-              Lake Mead <span className={styles.logo__text_span}>Dental</span>
-            </p>
-          </a>
-        </Link>
-        <div
-          className={`${styles.hamburger__menu} ${isOpen && styles.open}`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div className={styles.hamburger__menu_before}></div>
-          <div className={styles.hamburger__menu_present}></div>
-          <div className={styles.hamburger__menu_after}></div>
+        <div className={styles.navbar__container}>
+          <Link href='/'>
+            <a className={styles.logo}>
+              <div className={styles.logo__image}>
+                <Image src={logo} alt='logo' />
+              </div>
+              <p className={styles.logo__text}>
+                Lake Mead <span className={styles.logo__text_span}>Dental</span>
+              </p>
+            </a>
+          </Link>
+          <div
+            className={`${styles.hamburger__menu} ${isOpen && styles.open}`}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className={styles.hamburger__menu_before}></div>
+            <div className={styles.hamburger__menu_present}></div>
+            <div className={styles.hamburger__menu_after}></div>
+          </div>
+          <ul className={`${styles.navbar__list} ${isOpen && styles.show}`}>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/'>
+                <a className={styles.navbar__list_link}>Home</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/new-patients'>
+                <a className={styles.navbar__list_link}>New Patients</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/services'>
+                <a className={styles.navbar__list_link}>Services</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/meet-the-doctors'>
+                <a className={styles.navbar__list_link}>Meet The Doctors</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/insurance'>
+                <a className={styles.navbar__list_link}>Insurance</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/gallery'>
+                <a className={styles.navbar__list_link}>Gallery</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/covid-19'>
+                <a className={styles.navbar__list_link}>Covid 19</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/faq'>
+                <a className={styles.navbar__list_link}>FAQ</a>
+              </Link>
+            </li>
+            <li
+              className={styles.navbar__list_item}
+              onClick={() => setIsOpen(false)}
+            >
+              <Link href='/contact-us'>
+                <a
+                  className={`${styles.navbar__list_link} ${styles.navbar__list_link_last_child}`}
+                >
+                  Contact us
+                </a>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul className={`${styles.navbar__list} ${isOpen && styles.show}`}>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/'>
-              <a className={styles.navbar__list_link}>Home</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/new-patients'>
-              <a className={styles.navbar__list_link}>New Patients</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/services'>
-              <a className={styles.navbar__list_link}>Services</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/meet-the-doctors'>
-              <a className={styles.navbar__list_link}>Meet The Doctors</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/insurance'>
-              <a className={styles.navbar__list_link}>Insurance</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/gallery'>
-              <a className={styles.navbar__list_link}>Gallery</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/covid-19'>
-              <a className={styles.navbar__list_link}>Covid 19</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/faq'>
-              <a className={styles.navbar__list_link}>FAQ</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='/contact-us'>
-              <a
-                className={`${styles.navbar__list_link} ${styles.navbar__list_link_last_child}`}
-              >
-                Contact us
-              </a>
-            </Link>
-          </li>
-        </ul>
       </nav>
     </header>
   );
