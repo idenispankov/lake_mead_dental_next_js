5d11fd685130b8f9898cd79a9a3dcf6544755127
Denis Pankov <idenispankov@gmail.com>
2021-07-26 18:56:55 -0700

    contact info 768px

--- components/Header.js
+++ components/Header.js
@@ -3,23 +3,48 @@ import styles from '../styles/Header.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../public/images/logo_blue.png';
+import phoneIcon from '../public/images/phone_icon.svg';
+import mailIcon from '../public/images/mail_icon.svg';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <header className={styles.header}>
+      <div className={styles.contact__info}>
+        <p className={styles.contact__info_greeting}>
+          Welcome to Lake Mead Dental
+        </p>
+        <ul className={styles.contact__info_list}>
+          <li className={styles.contact__info_list_item}>
+            <div className={styles.contact__info_list_image}>
+              <Image src={phoneIcon} alt='phone icon' />
+            </div>
+            <a
+              className={styles.contact__info_list_item_link}
+              href='tel:7023041234'
+            >
+              (702) 304 - 1234
+            </a>
+          </li>
+          <li className={styles.contact__info_list_item}>
+            <div className={styles.contact__info_list_image}>
+              <Image src={mailIcon} alt='mail icon' />
+            </div>
+            <a
+              className={styles.contact__info_list_item_link}
+              href='mailto:lakemeaddental@gmail.com'
+            >
+              lakemeaddental@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
       <nav className={styles.navbar}>
         <Link href='/'>
           <a className={styles.logo}>
             <div className={styles.logo__image}>
-              <Image
-                src={logo}
-                alt='logo'
-                // width={26}
-                // height={26}
-                className={styles.logo__image}
-              />
+              <Image src={logo} alt='logo' />
             </div>
             <p className={styles.logo__text}>
               Lake Mead <span className={styles.logo__text_span}>Dental</span>
