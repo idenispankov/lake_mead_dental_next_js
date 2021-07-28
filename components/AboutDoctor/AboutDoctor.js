ee83642761eba8285a1baca97247a55421fa1c18
Denis Pankov <idenispankov@gmail.com>
2021-07-28 15:07:12 -0700

    change classnames in About doctor

--- components/AboutDoctor/AboutDoctor.js
+++ components/AboutDoctor/AboutDoctor.js
@@ -4,37 +4,39 @@ import Button from '../Button/Button';
 
 export default function MeetDoctor() {
   return (
-    <div className={styles.section}>
-      <span className={styles.section__image_dots} />
-      <span className={styles.section__image_lines} />
-      <div className={styles.section__image}></div>
-      <div className={styles.section__text_container}>
-        <h3 className={styles.section__heading}>Meet Dr. Ellen</h3>
-        <p className={styles.section__text}>
-          <span className={styles.section__text_span}>
-            Dr. Ellen Piyevsky, DDS
-          </span>
-          Lake Mead Dental & Wigwam Dental
-        </p>
-        <p className={styles.section__text}>
-          Dr Ellen graduated from USC School of Dentistry in 1995. She moved to
-          Las Vegas 4 years after graduating and opened the doors of Wigwam
-          Dental Care and Lake Mead Dental. Since then, she has been providing
-          quality dentistry to every single one of her patients. Her passion for
-          art and sculpture guides her into her love for cosmetic dentistry and
-          full mouth reconstruction.
-        </p>
-        <p className={styles.section__text}>
-          Dr Ellen says: "Our mission is to provide our patients with the most
-          positive dental experience possible, combined with personalized care
-          in a modern, comfortable environment. Learn More...
-        </p>
-        <Button
-          linkTo='/meet-the-doctors'
-          buttonText='Meet Our Doctors'
-          customButtonStyles={styles.button}
-        />
+    <section className={styles.section}>
+      <div className={styles.section__container}>
+        <span className={styles.section__container_image_dots} />
+        <span className={styles.section__container_image_lines} />
+        <div className={styles.section__container_image}></div>
+        <div className={styles.text__container}>
+          <h3 className={styles.text__container_heading}>Meet Dr. Ellen</h3>
+          <p className={styles.text__container_text}>
+            <span className={styles.text__container_text_span}>
+              Dr. Ellen Piyevsky, DDS
+            </span>
+            Lake Mead Dental & Wigwam Dental
+          </p>
+          <p className={styles.text__container_text}>
+            Dr Ellen graduated from USC School of Dentistry in 1995. She moved
+            to Las Vegas 4 years after graduating and opened the doors of Wigwam
+            Dental Care and Lake Mead Dental. Since then, she has been providing
+            quality dentistry to every single one of her patients. Her passion
+            for art and sculpture guides her into her love for cosmetic
+            dentistry and full mouth reconstruction.
+          </p>
+          <p className={styles.text__container_text}>
+            Dr Ellen says: "Our mission is to provide our patients with the most
+            positive dental experience possible, combined with personalized care
+            in a modern, comfortable environment. Learn More...
+          </p>
+          <Button
+            linkTo='/meet-the-doctors'
+            buttonText='Meet Our Doctors'
+            customButtonStyles={styles.button}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
