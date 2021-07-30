b6a86d7b4a1c7c40e1ce94ae6a60de9f4cb576ef
Denis Pankov <idenispankov@gmail.com>
2021-07-29 17:52:58 -0700

    taked component out of section tag

--- components/TreatmentsPageComponent/TreatmentsPageComponent.js
+++ components/TreatmentsPageComponent/TreatmentsPageComponent.js
@@ -3,24 +3,26 @@ import Treatments from '../../components/Treatments/Treatments';
 
 export default function TreatmentsPageComponent() {
   return (
-    <section className={styles.section}>
-      <div className={styles.section__container}>
-        <div className={styles.heading__container}>
-          <h2 className={styles.section__heading}>Our Treatments</h2>
-          <h3 className={styles.section__subheading}>
-            Many ways to better serve our Summerlin, Las Vegas Community
-          </h3>
-          <p className={styles.section__text}>
-            We want you to feel like you are part of our family. We strive to
-            create a comfortable, personal environment where we get to know each
-            and every patient. Every detail of our dental office is designed for
-            you, from our friendly dental team to our many amenities to make you
-            feel at home.
-          </p>
+    <>
+      <section className={styles.section}>
+        <div className={styles.section__container}>
+          <div className={styles.heading__container}>
+            <h2 className={styles.section__heading}>Our Treatments</h2>
+            <h3 className={styles.section__subheading}>
+              Many ways to better serve our Summerlin, Las Vegas Community
+            </h3>
+            <p className={styles.section__text}>
+              We want you to feel like you are part of our family. We strive to
+              create a comfortable, personal environment where we get to know
+              each and every patient. Every detail of our dental office is
+              designed for you, from our friendly dental team to our many
+              amenities to make you feel at home.
+            </p>
+          </div>
+          <div className={styles.section__image}></div>
         </div>
-        <div className={styles.section__image}></div>
-      </div>
+      </section>
       <Treatments />
-    </section>
+    </>
   );
 }
