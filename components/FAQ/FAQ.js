61c6334c1b2fb85d164e9fa7b427df0eb5866fe0
Denis Pankov <idenispankov@gmail.com>
2021-08-05 20:36:50 -0700

    FAQ page is ready - fully responsive

--- components/FAQ/FAQ.js
+++ components/FAQ/FAQ.js
@@ -2,8 +2,6 @@ import styles from './FAQ.module.css';
 import { useState } from 'react';
 
 export default function FAQ() {
-  // const [isOpen, setIsOpen] = useState(false);
-
   const [data, setData] = useState([
     {
       id: 1,
@@ -56,9 +54,51 @@ export default function FAQ() {
     },
     {
       id: 8,
-      question: 'Mouth injury due to accident at work',
+      question: 'Mouth injury due to accident at work?',
       answer:
         'Our extensive experience in accident and workers compensation cases allows us to process the necessary documentation, secure approval, and complete all dental procedures for the patient in minimal time. Please pick up the necessary paperwork with information about your workers compensation dental emergency  to bring with you to the initial appointment, or ask your human resources office to fax that information to us. Fax Number: 702-304-9499',
+      open: false,
+    },
+    {
+      id: 9,
+      question: 'What is a partial denture?',
+      answer:
+        'A Partial Denture is a removable appliance that replaces one or multiple missing teeth in the same jaw. There are several types of partial dentures that are available to patients. 1. Stay-plate, or temporary partial, is made out of acrylic with plastic teeth. It can replace all missing teeth in the same jaw, or just front ones for temporary aesthetics. 2. Metal Partial, silver or clear, is done for long-term use to replace partially missing teeth and restore chewing function to an optimum level. It is made of a metal skeleton that grasps the partial onto the remaining teeth with acrylic attached to it supporting plastic teeth. 3. Flexible partial, originally carrying the name Valplast after the company who introduced the concept in dentistry, is made purely out of plastic prepared under high temperatures. Over the years, this material evolved, giving patients many choices of colors to match any possible coloration of the gums. These partials are very durable besides being the primary choice for aesthetics.',
+      open: false,
+    },
+    {
+      id: 10,
+      question: 'What is a full denture?',
+      answer:
+        'A Full Denture is a removable appliance that replaces all of the missing teeth in the same arch. Therefore, a full denture procedure can be done to replace either all upper or all lower teeth. They are fabricated by the dental laboratory and made individually for every single patient. In case of complete loss of teeth, dentures allow patients to eat, speak, and smile with confidence. ',
+      open: false,
+    },
+    {
+      id: 11,
+      question: 'How is the denture repair process?',
+      answer:
+        'If your denture is broken, loose, or not fitting you properly, in most cases the denture can be repaired.To do so, you will come to our office as early as we open our doors. The doctor will evaluate your denture and will let you know what type of repair needs to be done. Then, we will proceed to take a mold of your mouth to send to the laboratory along with your denture. By the end of the day, you will come back to our office to have the repaired denture adjusted to your mouth. We will make sure there are no sharp edges and fits to your comfort. It is important to mention that as you start chewing with your newly repaired denture, you might get new sore spots. ​If this happens, you are welcome to come back to our office for extra adjustments for no extra charge.',
+      open: false,
+    },
+    {
+      id: 12,
+      question: 'What is the difference between different whitening products?',
+      answer:
+        'The most common procedure in aesthetic dentistry is Teeth Whitening. Over the years the market got oversaturated with different whitening products. 1. In-Home whitening kit is provided by the dentist. Custom made trays will be fabricated to hold the whitening gel on the teeth. The most important part about the In-Home whitening kit is to use it as instructed every single day without interruption. 2. In-Office whitening is usually done by a dental professional in the office. This procedure usually takes 2 - 2 ½ hours. At the end of the procedure, the results will be very similar to In-Home whitening. The type of the whitening must be chosen individually, so whether the choice is to use over-the-counter products or products that are given by the dentist, make an appointment for a consultation first.  Dentists can explain everything to help make a better choice. Please remember that whitening only works on the natural teeth. Crowns, veneers, filling, and any type of artificial teeth will not change the color.',
+      open: false,
+    },
+    {
+      id: 13,
+      question: 'Does Whitening make my teeth sensitive?',
+      answer:
+        'Every single whitening procedure gives tooth sensitivity. This is usually related to the dehydration of teeth.  This dehydration and sensitivity is temporary. Dentists can help get rid of that sensitivity much quicker by using concentrated fluoride treatment after whitening is completed.',
+      open: false,
+    },
+    {
+      id: 14,
+      question: 'What are the different types of veneers?',
+      answer:
+        'Veneers come in two different types: porcelain veneers and composite veneers. 1. Porcelain veneers are  custom made of glass particles by the dental laboratory individually. This procedure involves a couple of appointments, preparation, impression, and the 2nd appointment is actual cementation. 2. Composite veneers are made by the dentist as a sculpture while patient is in the dental chair using plastic-like filling material. In either procedure, the result can be amazing; however, due to the difference in the material glass vs. plastic, the lifespan and their glossiness can be different. Not every patient is a candidate for both, so please make an appointment with your dentist for consultation.',
       open: false,
     },
   ]);
@@ -78,10 +118,9 @@ export default function FAQ() {
 
   return (
     <section className={styles.section}>
-      <h1 className={styles.section__header}>F.A.Q</h1>
+      <h1 className={styles.section__heading}>F.A.Q</h1>
       <p className={styles.section__text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-        facere?
+        This page answers some of the frequently asked questions
       </p>
       <div className={styles.container}>
         {data.map((item) => {
@@ -128,35 +167,4 @@ export default function FAQ() {
       </div>
     </section>
   );
-}
-
-{
-  /* <div item={item} onClick={() => showAnswer(item.id)}>
-  <h2
-    className={`${styles.container__question} ${
-      item.open && styles.container__question_open
-    }`}
-  >
-    {item.question}{' '}
-    <div className={`${styles.button} ${item.open && styles.button_open}`}>
-      <div
-        className={`${styles.button__before} ${
-          item.open && styles.button__before_open
-        }`}
-      ></div>
-      <div
-        className={`${styles.button__present} ${
-          item.open && styles.button__present_open
-        }`}
-      ></div>
-    </div>
-  </h2>
-  <p
-    className={`${styles.container__answer} ${
-      item.open && styles.container__answer_open
-    }`}
-  >
-    {item.answer}
-  </p>
-</div>; */
 }
