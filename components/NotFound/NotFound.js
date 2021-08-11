e029449934a247bc6a81fa88ed3427071c34c5e0
Denis Pankov <idenispankov@gmail.com>
2021-08-10 19:16:41 -0700

    404 page created

---
+++ components/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import styles from './NotFound.module.css';
+import Image from 'next/image';
+import image from '../../public/images/not_found.jpg';
+
+export default function NotFound() {
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.section__heading}>404 Page</h1>
+      <div className={styles.image__container}>
+        <Image
+          src={image}
+          alt='dental tools'
+          width={700}
+          height={300}
+          layout='responsive'
+          className={styles.image}
+        />
+      </div>
+    </section>
+  );
+}
