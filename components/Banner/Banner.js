7e8984308c7e632e9d6d7e231771956e2796d37e
Denis Pankov <idenispankov@gmail.com>
2022-04-11 10:58:28 -0700

    banner title changed

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -9,8 +9,7 @@ export default function Header() {
           Own a Smile You Love to Share.
         </h1>
         <p className={styles.header__banner_text}>
-          {`Your Family Dentist In Summerlin, Las Vegas . We're Open During
-              Quarantine - We're Here For You!`}
+          {`Your Family Dentist In Summerlin, Las Vegas.`}
         </p>
         <Button
           linkTo='/treatments'
